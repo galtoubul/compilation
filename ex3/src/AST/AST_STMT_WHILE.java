fd6def6b38e1c1c6662fed2e5f6b6eddc7c8f33e
Eyal Nesher <eyal.nesher@gmail.com>
2021-12-11 00:01:04 +0200

    Implement semantics for while statements

--- ex3/src/AST/AST_STMT_WHILE.java
+++ ex3/src/AST/AST_STMT_WHILE.java
@@ -1,6 +1,8 @@
 package AST;
 
+import SYMBOL_TABLE.SYMBOL_TABLE;
 import TYPES.TYPE;
+import TYPES.TYPE_INT;
 
 public class AST_STMT_WHILE extends AST_STMT {
 	public AST_EXP cond;
@@ -15,6 +17,31 @@ public class AST_STMT_WHILE extends AST_STMT {
 	}
 
 	public TYPE SemantMe() {
-		return null; // TODO
+		/****************************/
+		/* [0] Semant the Condition */
+		/****************************/
+		if (cond.SemantMe() != TYPE_INT.getInstance()) {
+			System.out.format(">> ERROR [%d:%d] condition inside WHILE is not integral\n", 2, 2);
+		}
+
+		/*************************/
+		/* [1] Begin Loop Scope */
+		/*************************/
+		SYMBOL_TABLE.getInstance().beginScope();
+
+		/***************************/
+		/* [2] Semant Loop body */
+		/***************************/
+		body.SemantMe();
+
+		/*****************/
+		/* [3] End Scope */
+		/*****************/
+		SYMBOL_TABLE.getInstance().endScope();
+
+		/**************************************************/
+		/* [4] Return value is irrelevant for while loops */
+		/**************************************************/
+		return null;
 	}
 }
