816199b9813787a1d0621739e0a45944cff3308f
Eyal Nesher <eyal.nesher@gmail.com>
2021-12-10 03:38:37 +0200

    Implement SemantMe for assignment new

--- ex3/src/AST/AST_STMT_ASSIGN_NEW.java
+++ ex3/src/AST/AST_STMT_ASSIGN_NEW.java
@@ -13,7 +13,22 @@ public class AST_STMT_ASSIGN_NEW extends AST_STMT {
 
     @Override
     public TYPE SemantMe() {
-        // TODO Auto-generated method stub
+        System.out.format("ASSIGN");
+        TYPE t1 = null;
+        TYPE t2 = null;
+
+        // Retrieve children types
+        if (var != null)
+            t1 = var.SemantMe();
+        if (exp != null)
+            t2 = exp.SemantMe();
+
+        // Compate the two types
+        // TODO apply polymorphism
+        if (t1 != t2) {
+            System.out.format(">> ERROR [%d:%d] type mismatch for var := exp\n", 6, 6);
+        }
+
         return null;
     }
 }
