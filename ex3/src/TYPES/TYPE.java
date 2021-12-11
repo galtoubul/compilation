88fa88bfe7b51eb2a698fa1717feebbcd6fec022
Eyal Nesher <eyal.nesher@gmail.com>
2021-12-11 11:54:33 +0200

    Fix ancestor checking

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -20,9 +20,15 @@ public abstract class TYPE {
 		return this instanceof TYPE_ARRAY;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		return (obj instanceof TYPE) && this.name.equals(((TYPE) obj).name);
+	}
+
 	public boolean isSubtype(TYPE other) {
-		// TODO subtype
-		return this.name.equals(other.name) || TYPE_NIL.nil_match(other, this);
+		return this.equals(other) || TYPE_NIL.nil_match(other, this) ||
+				(this instanceof TYPE_CLASS && other instanceof TYPE_CLASS &&
+						((TYPE_CLASS) other).isAncestor((TYPE_CLASS) this));
 	}
 
 }
--- ex3/src/TYPES/TYPE_CLASS.java
+++ ex3/src/TYPES/TYPE_CLASS.java
@@ -1,37 +1,35 @@
 package TYPES;
 
-public class TYPE_CLASS extends TYPE
-{
+public class TYPE_CLASS extends TYPE {
 	/*********************************************************************/
-	/* If this class does not extend a father class this should be null  */
+	/* If this class does not extend a father class this should be null */
 	/*********************************************************************/
 	public TYPE_CLASS father;
 
 	/**************************************************/
-	/* Gather up all data members in one place        */
+	/* Gather up all data members in one place */
 	/* Note that data members coming from the AST are */
-	/* packed together with the class methods         */
+	/* packed together with the class methods */
 	/**************************************************/
 	public TYPE_LIST data_members;
-	
+
 	/****************/
 	/* CTROR(S) ... */
 	/****************/
-	public TYPE_CLASS(TYPE_CLASS father, String name, TYPE_LIST data_members)
-	{
+	public TYPE_CLASS(TYPE_CLASS father, String name, TYPE_LIST data_members) {
 		this.name = name;
 		this.father = father;
 		this.data_members = data_members;
 	}
 
 	public boolean isAncestor(TYPE_CLASS other) {
-		do  {
-			if(this.getClass().equals(other.getClass())) {
+		do {
+			if (this.equals(other)) {
 				return true;
 			}
 			other = other.father;
 		} while (other != null && other.father != null);
-		if(this.getClass().equals(other.getClass())) {
+		if (this.equals(other)) {
 			return true;
 		}
 		return false;
