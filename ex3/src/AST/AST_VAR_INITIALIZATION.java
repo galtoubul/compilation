66b970ad48a857afc7e2ad4af74a52f874999523
Eyal Nesher <eyal.nesher@gmail.com>
2021-12-12 10:13:09 +0200

    Limit field initialization to constant values

--- ex3/src/AST/AST_VAR_INITIALIZATION.java
+++ ex3/src/AST/AST_VAR_INITIALIZATION.java
@@ -3,6 +3,7 @@ package AST;
 import java.util.Optional;
 
 import SYMBOL_TABLE.SYMBOL_TABLE;
+import SYMBOL_TABLE.ScopeType;
 import TYPES.TYPE;
 
 public class AST_VAR_INITIALIZATION extends AST_VAR_DEC {
@@ -74,27 +75,13 @@ public class AST_VAR_INITIALIZATION extends AST_VAR_DEC {
             System.exit(0);
         }
 
-        /***************************************************/
-        /* [4] Enter the variable name to the Symbol Table */
-        /***************************************************/
-        SYMBOL_TABLE.getInstance().enter(id, t);
-
-        /************************************************************/
-        /* [5] Return value is irrelevant for variable declarations */
-        /************************************************************/
-        return null;
-    }
-
-    @Override
-    public TYPE SemantMe(String classId) {
-        TYPE t;
-
-        /****************************/
-        /* [1] Check If Type exists */
-        /****************************/
-        t = SYMBOL_TABLE.getInstance().find(type.name());
-        if (t == null) {
-            System.out.format(">> ERROR [%d:%d] non existing type %s\n", 2, 2, type.name());
+        // If the initialization is of a class field, it must be initialized with a
+        // constant
+        if (SYMBOL_TABLE.getInstance().currentScope() == ScopeType.Class &&
+                !(initialValue instanceof AST_EXP_INT) &&
+                !(initialValue instanceof AST_EXP_STRING) &&
+                !(initialValue instanceof AST_EXP_NIL)) {
+            System.out.format(">> ERROR [%d:%d] class fields can only be initialized with constant values\n", 2, 2, id);
             System.exit(0);
         }
 
