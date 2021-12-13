e3b354cb5b31f080616cda214bd0b60acf0f30e9
Asaf Rozin <rozinasaf@gmail.com>
2021-12-13 19:19:48 +0200

    added functions to TYPE_CLASS (by Eyal)

--- ex3/src/TYPES/TYPE_CLASS.java
+++ ex3/src/TYPES/TYPE_CLASS.java
@@ -1,5 +1,7 @@
 package TYPES;
 
+import java.util.Optional;
+
 public class TYPE_CLASS extends TYPE {
 	/*********************************************************************/
 	/* If this class does not extend a father class this should be null */
@@ -33,5 +35,35 @@ public class TYPE_CLASS extends TYPE {
 			return true;
 		}
 		return false;
+	}
+
+	// look for a member (field or method) name in the given TYPE_CLASS
+	// if found -> returns null. OW, returns null
+	private Optional<TYPE> lookupMember(String memberName) {
+		for (TYPE_LIST dataMembersList = this.data_members; dataMembersList != null; dataMembersList = dataMembersList.tail) {
+			System.out.println("-- AST_VAR_FIELD\n\t\tdata member name = " + dataMembersList.head.name);
+			System.out.println("-- AST_VAR_FIELD\n\t\tfieldName = " + memberName);
+
+			if (dataMembersList.head.name.equals(memberName)) {
+				return Optional.of(dataMembersList.head);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public Optional<TYPE> lookupMemberInAncestors(String memberName) {
+		TYPE_CLASS varTypeClass = this;
+		System.out.println("lookupMemberInAncestors varTypeClass.name = " + varTypeClass);
+
+		while (varTypeClass != null) {
+			Optional<TYPE> memberType = varTypeClass.lookupMember(memberName);
+			if (memberType.isPresent()) {
+				return memberType;
+			}
+
+			varTypeClass = varTypeClass.father;
+		}
+
+		return Optional.empty();
 	}
 }
